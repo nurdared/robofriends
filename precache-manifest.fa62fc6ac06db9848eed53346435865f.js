5fa24cc6259313bce16943dfe71bd67733e44149
nurdared <nurda9727@gmail.com>
2019-12-30 13:08:57 +0500

    Updates

---
+++ precache-manifest.fa62fc6ac06db9848eed53346435865f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc030c65dd250185b4afad73fb382b9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "31957ad7b35e9d767318",
+    "url": "/robofriends/static/css/main.68fb4353.chunk.css"
+  },
+  {
+    "revision": "f5dc7fdb4e44336c970d",
+    "url": "/robofriends/static/js/2.5faa7d2b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.5faa7d2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31957ad7b35e9d767318",
+    "url": "/robofriends/static/js/main.21428314.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
